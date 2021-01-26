92beeccd013755fb6b055e6cd9f23227c6761f37
cinyearchan <cinyearchan@gmail.com>
2021-01-26 14:31:51 +0800

    fix image url

--- code/shopping/src/components/cart.js
+++ code/shopping/src/components/cart.js
@@ -26,7 +26,7 @@ class Cart extends Component {
                 <div className="cart-item cart-column">
                   <img
                     className="cart-item-image"
-                    src={product.thumbnail}
+                    src={`http://localhost:3005${product.thumbnail}`}
                     width="100"
                     height="100"
                     alt=""
