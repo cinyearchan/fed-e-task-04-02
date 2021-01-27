cd884fc05af761f63b9f9ffe2d6fe9b645cbf946
cinyearchan <cinyearchan@gmail.com>
2021-01-27 14:35:12 +0800

    @action && bound this

--- code/mobx-guide/src/stores/CounterStore.js
+++ code/mobx-guide/src/stores/CounterStore.js
@@ -1,13 +1,17 @@
-import { observable } from 'mobx'
+import { observable, action, configure } from 'mobx'
+
+// 通过配置强制程序使用 action 函数更改应用程序中的状态
+// 如果不使用 action 装饰器，则无法更改状态
+configure({ enforceActions: 'observed' })
 
 class Counter {
   @observable count = 0
 
-  increment = () => {
+  @action.bound increment () {
     this.count = this.count + 1
   }
 
-  decrement = () => {
+  @action.bound decrement () {
     this.count = this.count - 1
   }
 }
